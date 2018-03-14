d1b64b3e92a302441f675f0f2fff973b63e0b762
goodkerwin <997186954@qq.com>
2018-03-14 11:58:03 +0800

    '管理资源'

--- webppack-demo/src/index.js
+++ webppack-demo/src/index.js
@@ -1,10 +1,20 @@
 import _ from 'lodash';
+import './css/style.css';
+import Icon from './images/equipments.png';
+import Data from './files/data.xml';
 
 function component() {
     var element = document.createElement('div');
 
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack!'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack!123'], ' ');
+    element.classList.add('hello');
+
+    var myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+
+    console.log(Data);
 
     return element;
 }
--- webppack-demo/webpack.config.js
+++ webppack-demo/webpack.config.js
@@ -5,5 +5,29 @@ module.exports = {
     output :{
         filename:'bundle.js',
         path:path.resolve(__dirname,"dist")
+    },
+    module:{
+        rules : [
+            {
+                test: /\.css$/,
+                use : ['style-loader','css-loader']
+            },//加载css
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
+            },//加载图片
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: ['file-loader']
+            },//加载字体
+            {
+                test: /\.(csv|tsv)$/,
+                use: ['csv-loader']
+            },
+            {
+                test: /\.xml$/,
+                use: ['xml-loader']
+            },//加载文件
+        ]
     }
 };
